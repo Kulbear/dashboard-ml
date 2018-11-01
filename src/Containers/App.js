e84cd0d3eedf88c4b88c9e62442a977e9ee51cba
Kulbear <jiyang.py@gmail.com>
2018-11-01 01:43:49 -0600

    Clean up the repo and setup Material UI

---
+++ src/Containers/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'typeface-roboto';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <h1>Hello World!</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
