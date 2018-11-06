29b6174d4e5224db704bc9cd48fe099b58f387de
Kulbear <jiyang.py@gmail.com>
2018-11-05 21:49:05 -0700

    Add Dashboard layout and styles

--- src/Containers/Dashboard.js
+++ src/Containers/Dashboard.js
@@ -2,14 +2,108 @@ import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import { dashboardStyles } from './UniversalStyleSheet';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { mainListItems, secondaryListItems } from '../Components/listItems';
+
 
 class Dashboard extends Component {
+  state = {
+    open: true,
+  };
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
   render () {
     const { classes } = this.props;
+
     return (
-      <div className={classes.root}>Placeholder...</div>
+      <React.Fragment>
+        <div className={classes.root}>
+          <AppBar
+            position="absolute"
+            className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
+          >
+            <Toolbar disableGutters={!this.state.open} className={classes.toolbar}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={this.handleDrawerOpen}
+                className={classNames(
+                  classes.menuButton,
+                  this.state.open && classes.menuButtonHidden,
+                )}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                component="h1"
+                variant="h6"
+                color="inherit"
+                noWrap
+                className={classes.title}
+              >
+                Dashboard
+              </Typography>
+              <IconButton color="inherit">
+                <Badge badgeContent={1} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
+            }}
+            open={this.state.open}
+          >
+            <div className={classes.toolbarIcon}>
+              <IconButton onClick={this.handleDrawerClose}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </div>
+            <Divider />
+            <List>{mainListItems}</List>
+            <Divider />
+            <List>{secondaryListItems}</List>
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Typography variant="h4" gutterBottom component="h2">
+              Experiments
+            </Typography>
+            <Divider />
+            <Typography variant="h4" gutterBottom component="h2">
+              Active Users
+            </Typography>
+          </main>
+        </div>
+      </React.Fragment>
     );
   }
 }
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(dashboardStyles)(Dashboard);
